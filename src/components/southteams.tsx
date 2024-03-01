dbe8c45e2b0bcc04d4e2aebf83d6cf7626713039
Mateus F dev <mateusfdev@outlook.com>
2024-03-01 16:13:00 -0300

    teams list finish 🥂🥇🥂 SEXTOOOU

--- src/components/southteams.tsx
+++ src/components/southteams.tsx
@@ -4,29 +4,70 @@ import Case from '../assets/teams/case.png'
 import Legacy from '../assets/teams/legacy.png'
 import Los from '../assets/teams/los.png'
 import Ninez from '../assets/teams/9z.png'
+import Liq from '../assets/teams/liquid.png'
+import Cloud from '../assets/teams/c9.png'
 
 export function Southteams() {
   return(
     <div className='
-    bg-stone-800 flex items-center rounded-md justify-between  mx-4  h-[240px]'>
-      <img
-      className='w-[80px] ml-10'
-       src={Case} alt="" />
-      <img
-      className='w-[120px]'
-       src={Legacy} alt="" />
-      <img
-      className='w-[140px]'
-       src={Furia} alt="" />
-      <img
-      className='w-[110px]'
-       src={Imp} alt="" />
-      <img
-     className='w-[140px]'
-       src={Los} alt="" />
-      <img
-    className='w-[140px] mr-10'
-       src={Ninez} alt="" />
+    bg-stone-800 flex items-center rounded-md justify-around  mx-4  h-[240px]'>
+      
+      <div className='flex ring-transparent hover:bg-stone-700/40 flex-col items-center  m-2 w-[360x] bg-stone-700/20 rounded-lg h-[200px] justify-center'>
+        <img
+        className='w-[70px] m-4 hover:transform hover:scale-125'
+        src={Los} 
+        />
+      </div>
+
+      <div className='flex ring-transparent hover:bg-stone-700/40 flex-col items-center  m-2 w-[360x] bg-stone-700/20 rounded-lg h-[200px] justify-center'>  
+        <img
+        className='w-10 m-4 hover:transform hover:scale-125'
+        src={Case} 
+        />
+       </div>
+
+       <div className='flex ring-transparent hover:bg-stone-700/40 flex-col items-center  m-2 w-[360x] bg-stone-700/20 rounded-lg h-[200px] justify-center'>  
+        <img
+        className='w-14 m-4 hover:transform hover:scale-125'
+        src={Legacy} 
+        />
+       </div>
+      
+      <div className='flex ring-transparent hover:bg-stone-700/40 flex-col items-center  m-2 w-[360x] bg-stone-700/20 rounded-lg h-[200px] justify-center'>
+        <img
+        className='w-[70px] m-4 hover:transform hover:scale-125'
+        src={Furia} 
+         />
+      </div>
+
+      <div className='flex ring-transparent hover:bg-stone-700/40 flex-col items-center  m-2 w-[360x] bg-stone-700/20 rounded-lg h-[200px] justify-center'>
+        <img
+        className='w-14 m-4 hover:transform hover:scale-125'
+        src={Imp} 
+        />
+      </div>
+
+
+      <div className='flex ring-transparent hover:bg-stone-700/40 flex-col items-center  m-2 w-[360x] bg-stone-700/20 rounded-lg h-[200px] justify-center'>
+        <img
+        className='w-[60px] m-4 hover:transform hover:scale-125'
+        src={Ninez} 
+        />
+      </div>
+  
+      <div className='flex ring-transparent hover:bg-stone-700/40 flex-col items-center  m-2 w-[360x] bg-stone-700/20 rounded-lg h-[200px] justify-center'>
+        <img
+        className='w-[60px] m-4 hover:transform hover:scale-125'
+        src={Liq}
+        />
+      </div>
+
+      <div className='flex ring-transparent hover:bg-stone-700/40 flex-col items-center  m-2 w-[360x] bg-stone-700/20 rounded-lg h-[200px] justify-center'>
+        <img
+        className='w-[60px] m-4 hover:transform hover:scale-125'
+        src={Cloud} 
+        />
+      </div>
   
       
     </div>
--- src/components/tiersteams.tsx
+++ src/components/tiersteams.tsx
@@ -1,37 +1,77 @@
-import Furia from '../assets/teams/c9.png'
-import Imp from '../assets/teams/vit.png'
-import Case from '../assets/teams/natus.png'
-import Legacy from '../assets/teams/virtus.png'
-import Los from '../assets/teams/heroic.png'
-import Ninez from '../assets/teams/faze.png'
+import Cloud from '../assets/teams/c9.png'
+import Vit from '../assets/teams/vit.png'
+import Navi from '../assets/teams/natus.png'
+import Virtus from '../assets/teams/virtus.png'
+import Heroic from '../assets/teams/heroic.png'
+import Faze from '../assets/teams/faze.png'
 import Spirit from '../assets/teams/Spirit.png'
+import Nip from '../assets/teams/nip.png'
+import Liq from '../assets/teams/liquid.png'
+import Ast from '../assets/teams/astr.png'
 
 export function TierSteams() {
   return(
     <div className='
     bg-stone-800 flex items-center rounded-md justify-between  mx-4  h-[240px]'>
-      <img
-      className='w-[80px] ml-10'
-       src={Case} alt="" />
-      <img
-      className='w-[120px]'
-       src={Legacy} alt="" />
-      <img
-      className='w-[140px]'
-       src={Furia} alt="" />
-      <img
-      className='w-[110px]'
-       src={Imp} alt="" />
-      <img
-     className='w-[140px]'
-       src={Los} alt="" />
-      <img
-    className='w-[140px] mr-10'
-       src={Ninez} alt="" />
-      <img
-    className='w-[140px] mr-10'
-       src={Spirit} alt="" />
-  
+    
+    <div className='flex ring-transparent hover:bg-stone-700/40 flex-col items-center  m-2 w-[360x] bg-stone-700/20 rounded-lg h-[200px] justify-center'>
+    <img
+   className='w-[70px] m-4 hover:transform hover:scale-125'
+       src={Nip} alt=""
+       />
+      </div>
+    
+    <div className='flex ring-transparent hover:bg-stone-700/40 flex-col items-center  m-2 w-[360x] bg-stone-700/20 rounded-lg h-[200px] justify-center'>
+
+    <img
+     className='w-[70px] m-4 hover:transform hover:scale-125'
+       src={Spirit} alt=""
+       />
+      </div>
+    
+    <div className='flex ring-transparent hover:bg-stone-700/40 flex-col items-center  m-2 w-[360x] bg-stone-700/20 rounded-lg h-[200px] justify-center'>
+
+    <img
+  className='w-[70px] m-4 hover:transform hover:scale-125'
+       src={Faze} alt=""
+       />
+      </div>
+    
+    <div className='flex ring-transparent hover:bg-stone-700/40 flex-col items-center  m-2 w-[360x] bg-stone-700/20 rounded-lg h-[200px] justify-center'>
+
+    <img
+  className='w-[70px] m-4 hover:transform hover:scale-125'
+       src={Heroic} alt=""
+       />
+      </div>
+    
+    <div className='flex ring-transparent hover:bg-stone-700/40 flex-col items-center  m-2 w-[360x] bg-stone-700/20 rounded-lg h-[200px] justify-center'>
+    <img
+  className='w-[70px] m-4 hover:transform hover:scale-125'
+       src={Virtus} alt=""
+       />
+      </div>
+    
+    <div className='flex ring-transparent hover:bg-stone-700/40 flex-col items-center  m-2 w-[360x] bg-stone-700/20 rounded-lg h-[200px] justify-center'>
+    <img
+   className='w-[70px] m-4 hover:transform hover:scale-125'
+       src={Navi} alt=""
+       />
+      </div>
+    
+    <div className='flex ring-transparent hover:bg-stone-700/40 flex-col items-center  m-2 w-[360x] bg-stone-700/20 rounded-lg h-[200px] justify-center'>
+    <img
+  className='w-[70px] m-4 hover:transform hover:scale-125'
+       src={Vit} alt=""
+       />
+      </div>
+    <div className='flex ring-transparent hover:bg-stone-700/40 flex-col items-center  m-2 w-[360x] bg-stone-700/20 rounded-lg h-[200px] justify-center'>
+    <img
+  className='w-[70px] m-4 hover:transform hover:scale-125'
+       src={Ast} alt=""
+       />
+      </div>
+
       
     </div>
   )
